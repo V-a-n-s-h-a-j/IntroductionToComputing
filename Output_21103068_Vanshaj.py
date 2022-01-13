7704b85ada4e74a6db6546922f0ed395f845ebc8
V-a-n-s-h-a-j <96770136+V-a-n-s-h-a-j@users.noreply.github.com>
2022-01-13 20:29:39 +0530

    Update Output_21103068_Vanshaj.py

--- Output_21103068_Vanshaj.py
+++ Output_21103068_Vanshaj.py
@@ -1,21 +1,26 @@
 Enter number 1 78
-Enter number 289
-Enter number 395
+Enter number 2 89
+Enter number 3 95
 87.33333333333333
+
 Enter gross income 500000
 Enter no. of dependants 3
 Income tax to be paid is : $ 96200.0
+  
 Enter SID 21103068
 Enter name Vanshaj
 Enter M if male, F if female, U if unknown M
 Enter course name CSE
 Enter CGPA 9.9
 [21103068, ' Vanshaj', ' M', ' CSE', 9.9]
+
 Enter marks of student 1 90
 Enter marks of student 285
 Enter marks of student 395
 Enter marks of student 4 99
 Enter marks of student 591
 [85.0, 90.0, 91.0, 95.0, 99.0]
+
 ['red', 'green', 'white', 'pink', 'yellow']
+
 ['red', 'green', 'white', 'purple']
